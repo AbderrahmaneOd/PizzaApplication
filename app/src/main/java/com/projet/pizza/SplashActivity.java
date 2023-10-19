3936a6bd00eb0d8e5921812a4ee1c14d2f1c9f80
Abderrahmane Ouaday <abdoouday361@gmail.com>
2023-10-19 13:37:32 +0100

    Add start screen

--- app/src/main/java/com/projet/pizza/SplashActivity.java
+++ app/src/main/java/com/projet/pizza/SplashActivity.java
@@ -4,13 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private ImageView logoImg;
+    private ImageView LoadingImg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        logoImg = findViewById(R.id.idLogo);
+        LoadingImg = findViewById(R.id.idLoadingIcon);
+
+        RotateAnimation anim = new RotateAnimation(0f, 360f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        anim.setInterpolator(new LinearInterpolator());
+        anim.setRepeatCount(Animation.INFINITE);
+        anim.setDuration(1000);
+
+        LoadingImg.startAnimation(anim);
 
         Thread t1 = new Thread(){
             @Override
